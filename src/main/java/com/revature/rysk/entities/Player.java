d3ad890235380c325d3e7460a467c3a2fbc0205b
Ian Mitchell <ian.mitchell@revature.net>
2021-12-11 15:08:16 -0700

    Cleaned up Player and add back CascadeTypes for AuthToken and Password

--- src/main/java/com/revature/rysk/entities/Player.java
+++ src/main/java/com/revature/rysk/entities/Player.java
@@ -4,12 +4,12 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
+@Setter
 @ToString
 public class Player {
     @Id
@@ -20,28 +20,15 @@ public class Player {
 
     @Column(nullable = false, unique = true)
     private String playerEmail;
+
     @Column(nullable = false)
     private String playerName;
-    @OneToOne(orphanRemoval = true)
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "auth_token_auth_token_id")
     private AuthToken authToken;
-    @ManyToOne
+
+    @ManyToOne(cascade = {CascadeType.ALL})
     @JoinColumn(name = "password_password_id")
     private Password password;
-
-    public AuthToken getAuthToken() {
-        return authToken;
-    }
-
-    public void setAuthToken(AuthToken authToken) {
-        this.authToken = authToken;
-    }
-
-    public Password getPassword() {
-        return password;
-    }
-
-    public void setPassword(Password password) {
-        this.password = password;
-    }
 }
