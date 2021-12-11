80a34fa37e2c6d23668c30486251dcdb7bcb6b43
Ian Mitchell <ian.mitchell@revature.net>
2021-12-11 15:06:23 -0700

    Add getter to AuthToken so can be properly returned with Player

--- src/main/java/com/revature/rysk/entities/AuthToken.java
+++ src/main/java/com/revature/rysk/entities/AuthToken.java
@@ -6,9 +6,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class AuthToken {
@@ -18,10 +18,14 @@ public class AuthToken {
     @Getter
     @Setter
     private long authTokenId;
+    @Getter
     @Setter
     @Column(unique = true)
     private String authToken;
 
+    public AuthToken() {
+        this.authToken = UUID.randomUUID().toString();
+    }
     private boolean checkAuthToken(String authTokenToCheck) {
         return authToken.equals(authTokenToCheck);
     }
