80537000a705e6356b8ddea8d5b06e1d3e2f0e0d
Ian Mitchell <ian.mitchell@revature.net>
2021-12-12 10:25:50 -0700

    Changed NoArgs Constructor to newDeck Method

--- src/main/java/com/revature/rysk/entities/Game/Deck.java
+++ src/main/java/com/revature/rysk/entities/Game/Deck.java
@@ -1,19 +1,32 @@
 package com.revature.rysk.entities.Game;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Deck {
     @Id
+    @GeneratedValue
     private long deckId;
 
     @Embedded
     private Stack<Card> cards;
 
-    public Deck() {
+    public void newDeck() {
         for (int i = 0; i < 14; i++) {
             this.cards.add(new Card(Card.TYPE.Cannon));
             this.cards.add(new Card(Card.TYPE.FootSoldier));
@@ -29,7 +42,7 @@ public class Deck {
         return this.cards.pop();
     }
 
-    public void discard(List<Card> discarded){
+    public void discard(List<Card> discarded) {
         cards.addAll(discarded);
         Collections.shuffle(this.cards);
     }
