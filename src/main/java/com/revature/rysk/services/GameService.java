75f76c9fb4f36993da85220e9f7a0b49cc94eba3
Ian Mitchell <ian.mitchell@revature.net>
2021-12-12 18:05:43 -0700

    Finish GameService interface

--- src/main/java/com/revature/rysk/services/GameService.java
+++ src/main/java/com/revature/rysk/services/GameService.java
@@ -1,5 +1,7 @@
 package com.revature.rysk.services;
 
+import com.revature.rysk.entities.Game.Card;
+import com.revature.rysk.entities.Game.Country;
 import com.revature.rysk.entities.Game.Game;
 import com.revature.rysk.entities.Player.Player;
 import org.springframework.stereotype.Service;
@@ -27,4 +29,15 @@ public interface GameService {
     //gets all log messages after specific message
     List<String> tailLog(Player player, long gameId, long logId);
 
+    int discard(List<Card> cards);
+
+    Country addArmies(Player player, long gameId, long countryId, int armies);
+
+    List<Integer> attack(Player player, long attackingCountryId, int attackingArmies, int numberOfDice, long defendingCountryId);
+
+    List<Integer> defend(Player player, long defendingCountryId, int numberOfDice);
+
+    Country move(Player player, long fromCountryId, long toCountryId, int numberOfArmies);
+
+    List<Card> draw(Player player);
 }
