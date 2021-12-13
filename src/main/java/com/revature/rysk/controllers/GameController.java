5b14e6be26ec6b242f7720ca298742b9b3333ecb
Ian Mitchell <ian.mitchell@revature.net>
2021-12-12 20:42:39 -0700

    Added controller for new game

--- src/main/java/com/revature/rysk/controllers/GameController.java
+++ src/main/java/com/revature/rysk/controllers/GameController.java
@@ -1,9 +1,24 @@
 package com.revature.rysk.controllers;
 
+import com.revature.rysk.entities.Game.Game;
+import com.revature.rysk.entities.Player.Player;
+import com.revature.rysk.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GameController {
+    @Autowired
+    GameService gameService;
 
+    @PostMapping("/game/new")
+    public Game newGame(@RequestBody() List<Player> players) {
+        System.out.println(players);
+        return gameService.newGame(players);
+    }
 
 }
