1cfb2930a3d26e966160db943afef0e8f52b0067
Ian Mitchell <ian.mitchell@revature.net>
2021-12-22 08:05:45 -0700

    Fixed new JS Register Component

--- rysk-frontend-js/src/App.js
+++ rysk-frontend-js/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import Register from "./Components/Pages/Register";
 import { updatePlayer } from "./features/player/playerSlice";
 
 function App() {
@@ -13,15 +14,17 @@ function App() {
     playerName: "Name",
     playerAuthToken: "token",
     playerPassword: "password",
-  }
-  dispatch(updatePlayer(newPlayer))
+  };
+  //dispatch(updatePlayer(newPlayer));
 
-  console.log(loggedInPlayer)
-  
-  return <div className="App">
-    The player is {loggedInPlayer.playerName}
-    
-  </div>;
+  console.log(loggedInPlayer);
+
+  return (
+    <div className="App">
+      The player is {loggedInPlayer.playerName}
+      <Register />
+    </div>
+  );
 }
 
 export default App;
